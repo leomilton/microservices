82cc61e97a8f9b79ead834cc7b57720625e250cb
Leo Milton <leo_milton@epam.com>
2026-02-02 16:21:30 +0530

    Add Docker DNS

--- micro-collector/src/main/java/com/epam/microcollector/scheduler/CollectorScheduler.java
+++ micro-collector/src/main/java/com/epam/microcollector/scheduler/CollectorScheduler.java
@@ -3,6 +3,7 @@ package com.epam.microcollector.scheduler;
 import com.epam.microcollector.dto.NotificationMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +17,12 @@ public class CollectorScheduler {
     private static final Logger log = LoggerFactory.getLogger(CollectorScheduler.class);
 
     private final RestTemplate restTemplate;
+    private final String recipientBaseUrl;
 
-    public CollectorScheduler(RestTemplate restTemplate) {
+    public CollectorScheduler(RestTemplate restTemplate,
+                              @Value("${recipient.base-url}") String recipientBaseUrl) {
         this.restTemplate = restTemplate;
+        this.recipientBaseUrl = recipientBaseUrl;
     }
 
     @Scheduled(fixedRate = 10000) // M = 10 seconds ( > N )
@@ -27,7 +31,7 @@ public class CollectorScheduler {
         log.info("CollectorScheduler started");
 
         NotificationMessage[] response =
-                restTemplate.getForObject("http://localhost:8081/message", NotificationMessage[].class);
+                restTemplate.getForObject(recipientBaseUrl  + "/message", NotificationMessage[].class);
 
         if (response == null || response.length == 0) {
             log.info("No messages received from micro-recipient");
--- micro-collector/src/test/java/com/epam/microcollector/MicroCollectorApplicationTests.java
+++ micro-collector/src/test/java/com/epam/microcollector/MicroCollectorApplicationTests.java
@@ -2,8 +2,12 @@ package com.epam.microcollector;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureWireMock(port = 0)
 class MicroCollectorApplicationTests {
 
 	@Test
